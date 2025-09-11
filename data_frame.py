fe7811ae5f1ddd0fa9b79389a01053dde30ab891
Benjamin Delacruz <bydelacruz@icloud.com>
2025-09-10 21:50:13 -0400

    added further notes and examples in using methods like max() and describe(). Also how o access columns using bracket notation

--- data_frame.py
+++ data_frame.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
-# manually storing data in a table by using the DataFrame method
+"""
+manually storing data in a table by using the DataFrame method
+the DataFrame method takes a Dict as argument. each key represents a column.
+the values of those keys are lists where each item represents a row.
+"""
+
 df = pd.DataFrame(
     {
         "Name": [
@@ -13,9 +18,28 @@ df = pd.DataFrame(
     }
 )
 
-print(df)
+# print(df)
 
 """
-the DataFrame method takes a Dict as argument. each key represents a column.
-the values of those keys are lists where each item represents a row.
+ Each column in a DataFrame is called a series
+ we can target individual series (or column) by using bracket notation
 """
+
+# print(df["Age"])
+
+"""
+ you can also create a Series from scratch. like this:
+"""
+ages = pd.Series([22, 35, 58], name="Age")
+# print(ages)
+
+"""
+there are methods we can call like max() to get max number
+"""
+
+# print(df["Age"].max())
+
+"""
+there are also methods to gather numerical data like count, mean, std, etc 
+"""
+# print(df.describe())
