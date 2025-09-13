bc767d462f6a70fab0fdacb286271a2214235960
Benjamin Delacruz <bydelacruz@icloud.com>
2025-09-13 08:51:53 -0400

    initial commit

---
+++ reshaping_table_layout.py
@@ -0,0 +1,6 @@
+# how to rehshape a table layout to do things like sort columns, hide columns, and change column widths
+
+import pandas as pd
+
+titanic = pd.read_csv("titanic.csv")
+air_quality = pd.read_csv("air_quality_no2.csv")
